828a02ebd5453c2439f1df409e4c403cae8d2036
Michiel Coulier <michielcoulier@gmail.com>
2020-11-09 14:36:28 +0100

    shows unknown, routing done

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -5,50 +5,89 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 class LearningController extends AbstractController
 {
+    private $session;
+    private string $name = "Unknown";
+
+//Construct session
+    public function __construct(SessionInterface $session)
+    {
+        $this->session= $session;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
-    public function homePage(): Response
+/*    public function homePage(): Response
     {
         return $this->redirectToRoute('show-my-name');
-    }
+    }*/
+
     /**
      * @Route("/learning", name="learning")
      */
-    public function index(): Response
+/*    public function index(): Response
     {
         return $this->render('learning/index.html.twig', [
             'controller_name' => 'LearningController',
         ]);
-    }
+    }*/
+
+
+    //If name is filled in, return the aboutMe page with name. Else forward to homepage.
     /**
-     * @Route("/about-me", name="about-me")
+     * @Route("/about-becode", name="about-me")
      */
     public function aboutMe(): Response
     {
-        return $this->render('learning/about-me.html.twig', [
-            'controller_name' => 'LearningController',
-        ]);
+        if($this->session->get('name')){
+            $name = $this->session->get('name');
+            return $this->render('learning/about-me.html.twig', [
+                'name' => $name,
+            ]);
+        } else {
+            return $this->forward('App\Controller\LearningController::showMyName');
+        }
     }
+
+    //Show name if session exists, else show unknown.
     /**
-     * @Route("/show-my-name", name="show-my-name")
+     * @Route("/", name="show-my-name")
      */
     public function showMyName(): Response
     {
+        if($this->session->get('name')){
+            $name = $this->session->get('name');
+/*            return $this->render('learning/show-my-name.html.twig', [
+                'name' => $name,
+            ]);*/
+        } else {
+            $name = "unknown";
+/*            return $this->render('learning/show-my-name.html.twig', [
+                'name' => $name,
+            ]);*/
+        }
         return $this->render('learning/show-my-name.html.twig', [
-            'controller_name' => 'LearningController',
+            'name' => $name,
         ]);
     }
+
+    //Get name with POST, redirect to show-my-name & route is POST
     /**
-     * @Route("/change-my-name", name="change-my-name")
+     * @Route("/change-my-name", name="change-my-name", methods={"POST"})
+     * @return RedirectResponse
      */
     public function changeMyName(): Response
     {
-        return $this->render('learning/change-my-name.html.twig', [
-            'controller_name' => 'LearningController',
+        $this->session->set('name', $_POST['name']);
+        $this->render('learning/change-my-name.html.twig', [
+            'name' => $_POST['name'],
         ]);
+        return $this->redirectToRoute('show-my-name');
     }
 }
